cd4e39e8334347dd0a684f9d437415488ef02528
HeylonNHP <heylon96@hotmail.com>
2018-10-21 02:02:05 +1000

    Added MySQL database driver

--- www-weather/src/Main.java
+++ www-weather/src/Main.java
@@ -5,5 +5,11 @@
 public class Main {
     public static void main(String args[]){
         System.out.print("Hello world!");
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
