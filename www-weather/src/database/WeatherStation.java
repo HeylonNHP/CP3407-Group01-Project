1d9065a9e2060b38aa3402d6ee7f7a3f4604ad09
HeylonNHP <heylon96@hotmail.com>
2018-10-22 14:58:44 +1000

    Created WeatherStation class

---
+++ www-weather/src/database/WeatherStation.java
@@ -0,0 +1,37 @@
+package database;
+
+public class WeatherStation {
+    int stationID;
+    String stationName;
+    int stationPowerStatus;
+
+    public WeatherStation(int ID, String Name, int PowerStatus){
+        setStationID(ID);
+        setStationName(Name);
+        setStationPowerStatus(PowerStatus);
+    }
+
+    public int getStationID() {
+        return stationID;
+    }
+
+    public void setStationID(int stationID) {
+        this.stationID = stationID;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public int getStationPowerStatus() {
+        return stationPowerStatus;
+    }
+
+    public void setStationPowerStatus(int stationPowerStatus) {
+        this.stationPowerStatus = stationPowerStatus;
+    }
+}
