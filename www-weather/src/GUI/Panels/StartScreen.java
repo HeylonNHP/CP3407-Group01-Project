237f195aa7d46048f89150246bd03542111c6021
HeylonNHP <heylon96@hotmail.com>
2018-10-22 20:22:10 +1000

    StartScreen panel created

---
+++ www-weather/src/GUI/Panels/StartScreen.java
@@ -0,0 +1,61 @@
+package GUI.Panels;
+
+import GUI.Window;
+import com.sun.glass.ui.Size;
+
+import javax.naming.ldap.Control;
+import javax.swing.*;
+import java.awt.*;
+
+public class StartScreen extends JPanel {
+    public StartScreen(){
+        //BoxLayout layout = new BoxLayout(this,BoxLayout.PAGE_AXIS);
+        //setLayout(layout);
+        //setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+        setLayout(null);
+        System.out.println(Window.getSize());
+        Dimension size = Window.getSize();
+        //setSize(size);
+        setPreferredSize(size);
+
+
+        Dimension windowSize = Window.getSize();
+
+        //Login button
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(windowSize.width - 150, 50,100,25);
+        add(loginButton);
+
+        //Menu panel
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new GridBagLayout());
+        menuPanel.setBounds((windowSize.width/2) - 150, 100,300, 300);
+        menuPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Menu"));
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        JButton weatherStations = new JButton("Weather Stations");
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.BOTH;
+        c.weighty = 1;
+        c.weightx = 1;
+
+        menuPanel.add(weatherStations,c);
+
+        JButton management = new JButton("Management");
+        c.gridy = 1;
+        menuPanel.add(management,c);
+
+        JButton stationMaintenance = new JButton("Station maintenance");
+        c.gridy = 2;
+        menuPanel.add(stationMaintenance,c);
+
+        add(menuPanel);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
--- www-weather/src/GUI/Window.java
+++ www-weather/src/GUI/Window.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import GUI.Panels.StartScreen;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,17 +9,28 @@ public class Window {
     private static JFrame mainWindow = new JFrame();
     static {
         //initialise window
-        LayoutManager layout = new FlowLayout();
+        LayoutManager layout = new BorderLayout();
         mainWindow.setLayout(layout);
         mainWindow.setTitle("Www-weather monitor");
         mainWindow.setSize(700,500);
         mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainWindow.setResizable(true);
+        mainWindow.setResizable(false);
 
 
     }
 
     public static void showWindow(){
+        showStartScreen();
         mainWindow.setVisible(true);
+    }
+
+    public static void showStartScreen(){
+        mainWindow.add(new StartScreen(),BorderLayout.CENTER);
+        mainWindow.revalidate();
+        mainWindow.repaint();
+    }
+
+    public static Dimension getSize(){
+        return mainWindow.getSize();
     }
 }
